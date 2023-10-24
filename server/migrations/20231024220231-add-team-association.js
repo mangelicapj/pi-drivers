b8e89ed1b42de27fa1d1cd269a022ed2ec338497
mangelicapj <mangelica.0894@gmail.com>
2023-10-24 17:30:02 -0500

    base de datos

---
+++ server/migrations/20231024220231-add-team-association.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Drivers', 'TeamId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Teams',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Drivers', 'TeamId');
+  },
+};
--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -2,11 +2,37 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
     name: {
+      type: DataTypes.STRING,
+    },
+    surname: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+    },
+    dob: {
+      type: DataTypes.DATE,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
   });
+  Driver.belongsTo(Team); 
+
+  return Driver;
 };
+
+
