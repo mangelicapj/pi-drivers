23b442ad2910df7542680acc6aedcc8cd181126e
mangelicapj <mangelica.0894@gmail.com>
2023-10-25 10:34:29 -0500

    base de datos

---
+++ server/migrations/20231025152656-reset-teams-up.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // No es necesario escribir nada aquí si solo quieres recrear la tabla "Teams".
+  },
+};
